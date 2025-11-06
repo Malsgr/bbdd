52a012e3b7ed8d6b28c3d86c296624c3b9498a4a
Malsgr <tomaspmdc@gmail.com>
2025-11-06 19:53:04 +0100

    Update insertar-peliculas.sql

--- tareas/1-monotablas-sqlite/insertar-peliculas.sql
+++ tareas/1-monotablas-sqlite/insertar-peliculas.sql
@@ -1,1 +1,7 @@
-INSERT INTO peliculas.csv(id,title,director,year,rating) FROM
+INSERT INTO peliculas (id, title, director, year, rating)
+VALUES
+(1, "Toy Story", "John Lasseter", 1995, 6.7)
+(2, "Jumanji", "Joe Johnston", 1995, 6.7)
+(3, "Grumpier Old Men", "Howard Deutch", 1995, 6.7)
+(4, "Waiting to Exhale", "Forest Whitaker", 1995, 6.7)
+(5, "Father of the Bride Part II", "Charles Shyer", 1995, 6.7)
