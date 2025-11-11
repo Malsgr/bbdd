84ad1aa6039962621c65c8d28b031d8cb978476c
Malsgr <tomaspmdc@gmail.com>
2025-11-11 08:29:50 +0000

    correccíon sintaxis insertar-productos.sql

--- tareas/1-monotablas-sqlite/insertar-productos.sql
+++ tareas/1-monotablas-sqlite/insertar-productos.sql
@@ -1,7 +1,8 @@
 INSERT INTO TABLE productos (id, name, price, stock, sale)
 VALUES
-(1, "Manzanas", 40.99, 145, 15%)
-(2, "Caza a reacción de cambate F-22 Raptor", 5.89, 2, 98%)
-(3, "Pelota antiestrés Amazon", 123, 456, 10%)
-(4, "Tarjeta de regalo Spotify", 1099.80, 0, 0%)
-(5, "Sarcófago Egipcio, Buen estado, No se hacen envios", 20, 1, 714562%);
+(1, "Manzanas", 40.99, 145, 15%),
+(2, "Caza a reacción de cambate F-22 Raptor", 5.89, 2, 98%),
+(3, "Pelota antiestrés Amazon", 123, 456, 10%),
+(4, "Tarjeta de regalo Spotify", 1099.80, 0, 0%),
+(5, "Sarcófago Egipcio, Buen estado, No se hacen envios", 20, 1, 714562%)
+;
